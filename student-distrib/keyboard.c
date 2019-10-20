d3d301a973ed4ab98784db3dcadd5ed6de54e174
Cakulev <cakulev2@illinois.edu>
2019-10-20 18:39:21 -0500

    key board working - big brugma moment

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,25 +1,40 @@
+/* keyboard.c - Functions to display character to screen
+Author - Ian Goodwin
+https://wiki.osdev.org/PS2_Keyboard
+ */
+
 #include "keyboard.h"
 #include "i8259.h"
 #include "lib.h"
-scancod_lowercase[]
+char scancode_lowercase[] = {0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+                            '-', '=', 0, 0, 'q', 'w', 'e', 'r', 't', 'y', 'u',
+                            'i', 'o', 'p', '{', '}', '\n', 0, 'a', 's', 'd', 'f','g',
+                            'h', 'j', 'k', 'l', ';', 0x27, 0x60, 0, 0x5c, 'z', 'x', 'c',
+                            'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' ', 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '7', '8', '9', '-', '4',
+                            '5', '6', '+', '1', '2', '3', '0', '.'};
+
 //open keyboard Interrupt on port 1
 void open_keyboard_irq(){
   enable_irq(1);
+  clear();
   return;
 }
 
 void keyboard_handler(){
+  char c;
   cli();
-  clear();
   asm volatile(
     "pushal\n"
     :
     :);
-  char c;
-  c = inb(DATAPORT);
 
-  printf("brug\n");
-  printf("%d",c);
+  c = inb(DATAPORT);
+  if((c & 0x80) == 0)
+  {
+    c = scancode_lowercase[c];
+    putc(c);
+  }
   sti();
   send_eoi(1);
   asm volatile(
