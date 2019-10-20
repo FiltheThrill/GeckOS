453f40953d6fb5efbaa6f72065d0f71b7c641f07
Cakulev <cakulev2@illinois.edu>
2019-10-19 20:50:46 -0500

    working exception handler

--- student-distrib/IDT.h
+++ student-distrib/IDT.h
@@ -1,11 +1,13 @@
 //descriptors for idtc.c - INCLUDES EXCEPTION_FLAG WHICH WILL PROBABLY BE USED IN OTHER
 //FILES. sSimply include IDT.h to get access to exception_flag
-
+#ifndef _IDT_H
+#define _IDT_H
 
 #define EXCEPTIONS_AMOUNT   32        //first 32 in idt
-#define MASTER_AND_SLAVE    48        //each PIC take sup 1 byte in idt
-#define SYS_CALL_VECT       128       // interrupt vector for system calls
+#define MASTER_AND_SLAVE    48        //each PIC takes up 1 byte in idt
+#define SYS_CALL_VECT       0x80       // interrupt vector for system calls
 #define IRQ8                40        //real time clock
+#define KEYBOARD_IRQ        0x21
 
 int exception_flag;
 
@@ -24,13 +26,13 @@ extern void seg_not_present();
 extern void stack_seg_fault();
 extern void general_protection();
 extern void page_fault();
-extern void reserved0(;
+extern void reserved0();
 extern void floating_point();
 extern void alignment_check();
 extern void machine_check();
 extern void simd_floating_point();
 extern void virtualization();
-extern void reserved1();}
+extern void reserved1();
 extern void reserved2();
 extern void reserved3();
 extern void reserved4();
@@ -43,3 +45,6 @@ extern void security();
 extern void reserved10();
 
 extern void exceptions(int exception_num);
+
+extern void idt_init();
+#endif
--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -21,7 +21,7 @@ static inline void assertion_failure(){
 /* Checkpoint 1 tests */
 
 /* IDT Test - Example
- * 
+ *
  * Asserts that first 10 IDT entries are not NULL
  * Inputs: None
  * Outputs: PASS/FAIL
@@ -35,7 +35,7 @@ int idt_test(){
 	int i;
 	int result = PASS;
 	for (i = 0; i < 10; ++i){
-		if ((idt[i].offset_15_00 == NULL) && 
+		if ((idt[i].offset_15_00 == NULL) &&
 			(idt[i].offset_31_16 == NULL)){
 			assertion_failure();
 			result = FAIL;
@@ -45,6 +45,16 @@ int idt_test(){
 	return result;
 }
 
+int div_by_0_test()
+{
+	TEST_HEADER;
+	
+	int i;
+	int result = PASS;
+	i = 1/0;
+	return result;
+
+}
 // add more tests here
 
 /* Checkpoint 2 tests */
@@ -56,5 +66,6 @@ int idt_test(){
 /* Test suite entry point */
 void launch_tests(){
 	TEST_OUTPUT("idt_test", idt_test());
+	TEST_OUTPUT("Divide by 0 test", div_by_0_test());
 	// launch your tests here
 }
