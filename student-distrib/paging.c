1adc534e667405fb1f6edb79ea7c7876b6d5b670
Papa <fpapa2@illinois.edu>
2019-10-20 17:56:39 -0500

    Prior to merging

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -3,17 +3,15 @@ Author - Francis Papa
 https://wiki.osdev.org/Paging
  */
  #include "paging.h"
+ #include "lib.h"
  
  /* Align page directory and our first page table to 4KB */
- uint32_t page_directory[NUM_ENTRIES] __attribute__((aligned(KB_FOUR)));
- uint32_t page_table[NUM_ENTRIES] __attribute__((aligned(KB_FOUR)));
+ uint32_t page_directory[NUM_ENTRIES] __attribute__((aligned(FOUR_KB)));
+ uint32_t page_table[NUM_ENTRIES] __attribute__((aligned(FOUR_KB)));
  
  void paging_init()
  {
 	 int i;
-	 
-	 /* Enable paging */
-	 paging_enable();
 	 
 	 /* Set all PDEs and PTEs for the first page table to not present */
 	 for (i = 0; i < NUM_ENTRIES; i++)
@@ -22,36 +20,43 @@ https://wiki.osdev.org/Paging
 		 page_table[i] = NOT_PRESENT;
 	 }
 	 
-	 page_directory[KERNEL_ADDR] = KERNEL_PAGE_INIT;
-	 page_table[VIDMEM_ADDR] = VIDMEM_PAGE_INIT;
+	 //page_directory[KERNEL_ADDR] = KERNEL_PAGE_INIT;
+	 //page_table[VIDMEM_ADDR] = VIDMEM_PAGE_INIT;
 	 page_directory[0] = page_table;
+	 page_directory[0] |= PRESENT;
+	 
+	 /* Enable paging */
+	 //paging_enable();
 	 
  }
  
  void paging_enable()
  {
-	 asm volatile (
-        "movl page_directory, %%eax							;"
-       	"movl %%eax, %%cr3									;"
-       	"movl %%cr0, %%eax									;"
-		"orl $PAGING_EN, %%eax								;"
-		"movl %%eax, %%cr0									;"
-		"movl %%cr4, %%eax									;"
-		"orl $PSE_EN, %%eax									;"
-		"movl %%eax, %%cr4									;"
-      : /* no outputs */
-      : 
-      :
+	asm volatile ("                 		\n\
+            movl %0, %%eax     				\n\
+            movl %%eax, %%cr3			    \n\
+            movl %%cr0, %%eax			    \n\
+            orl %1, %%eax	       			\n\
+            movl %%eax, %%cr0			    \n\
+            movl %%cr4, %%eax			    \n\
+            orl %2, %%eax		        	\n\
+            movl %%eax, %%cr4		        \n\
+            "
+			: /* no outputs */
+			: "r" (page_directory), "r" (PAGING_EN), "r" (PSE_EN)
+			: "eax"
     );
  }
  
  void flush_tlb()
  {
-	 asm volatile (
-		"movl %%cr3, %%eax									;"
-		"movl %%eax, %%cr3									;"
-		:
-		:
-		:
-	 );
+	 asm volatile ("                 		\n\
+            movl %%cr3, %%eax  				\n\
+            movl %%eax, %%cr3			    \n\
+            "
+			: /* no outputs */
+			: /* no inputs  */
+			: "eax"
+    );
  }
+ 
--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -1,15 +1,16 @@
 #ifndef _PAGING_H
 #define _PAGING_H
 
-#define NUM_ENTRIES	1024
-#define KB_FOUR	4096
-#define PAGING_EN 0x80000001
-#define PSE_EN	0x00000010
-#define NOT_PRESENT	0x00000002
-#define KERNEL_ADDR	0x400000
-#define VIDMEM_ADDR 0xB8000
-#define KERNEL_PAGE_INIT 0x00000193
-#define VIDMEM_PAGE_INIT 0x00000003
+#define NUM_ENTRIES			1024
+#define FOUR_KB				4096
+#define PAGING_EN 			0x80000001
+#define PSE_EN				0x00000010
+#define NOT_PRESENT			0x00000002
+#define PRESENT				0x00000003
+#define KERNEL_ADDR			0x400000
+#define VIDMEM_ADDR 		0xB8000
+#define KERNEL_PAGE_INIT 	0x00000193
+#define VIDMEM_PAGE_INIT 	0x00000003
 
 void paging_init();
 void paging_enable();
