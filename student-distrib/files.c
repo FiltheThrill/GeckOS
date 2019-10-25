a61a3438cbe2bca643ec008d4b03eff6446e5b59
Cakulev <cakulev2@illinois.edu>
2019-10-24 23:18:34 -0500

    only fread and dread missing

--- student-distrib/files.c
+++ student-distrib/files.c
@@ -6,10 +6,11 @@ https://littleosbook.github.io/#inodes-and-writable-file-systems
 diagram in appendix A in mp3 documentation is hella clutch as well*/
 
 #include "files.h"
+#include "lib.h"
 
 boot_block_t* boot_block;
 inode_t* inode_addr;
-
+dentry_t global_dentry;
 void files_init(boot_block_t* start)
 {
   //first 4kb block is accopied by boot block. Inode addresses start thereafter
@@ -24,12 +25,14 @@ int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry)
   if(fname == 0){   //if pointer to file is null return -1
     return FAILURE;
   }
-  for(i = 0; i < FILESMAX; i++)   //loop through file names to see if it can be found
+  //loop through file names to see if it can be found
+  for(i = 0; i < FILESMAX; i++)
   {
-    found = strncmp((const uint8_t)fname, (const uint8_t*)boot_block->dir_entries[i].file_name, RESERVED32B);
+    found = strncmp((const int8_t*)fname, (const int8_t*)boot_block->dir_entries[i].file_name, RESERVED32B);
     if(found == 0)
     {
-      memcpy(dentry, (const void*)boot_lock->dir_entries[i], SIXTYFOURBYTES);  //copy over to dentry
+      //copy over to dentry
+      memcpy(dentry, (const void*)&boot_block->dir_entries[i], SIXTYFOURBYTES);
       return SUCCESS;
     }
   }
@@ -42,16 +45,20 @@ int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry)
   {
     return FAILURE;
   }
-  memcpy(dentry, (const void*)boot_lock->dir_entries[index], SIXTYFOURBYTES);  //copy over by index to dentry
+  //copy over by index to dentry
+  memcpy(dentry, (const void*)&boot_block->dir_entries[index], SIXTYFOURBYTES);
   return SUCCESS;
 }
 
+//bruh wtf
 int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length)
 {
-  int32_t cpy_length, inode_len, inode_num;
+  int32_t cpy_length, inode_len, inode_num, offset_blocks, offset_bytes, data_idx;
+  int bytes_read = 0;
+  int i = 0;
 
   inode_len = inode_addr[inode].length_in_B;
-  inode_num =  boot_lock->num_inodes;
+  inode_num =  boot_block->num_inodes;
 
   if((inode < 0) || (inode >= inode_num))  // check if inode is in bounds
   {
@@ -61,7 +68,16 @@ int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length
   {
     return FAILURE;
   }
+  if(offset >= inode_len){    //return 0 bytes read if offset it too large
+    return bytes_read;
+  }
 
+  //values for indexing
+  uint8_t* data_blocks = (uint8_t*)(inode_addr + inode_num);  //first data block address (?)
+  offset_blocks = offset / FOURKB;  //number of complete blocks
+  offset_bytes = offset % FOURKB;    //number of bytes into the last block
+
+  //check to see if offset + length is more bytes than the file has
   if(offset + length <= inode_len)
   {
     cpy_length = length;
@@ -73,6 +89,83 @@ int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length
 
   while(cpy_length > 0)
   {
-
+    //get info on which data block we are reading from
+    data_idx = inode_addr[inode].data_block_idx[offset_blocks];
+    while(offset_bytes < FOURKB)
+    {
+      //4kb * #blocks + offset bytes + starting address for data blocks
+      buf[i] = *(data_blocks + offset_bytes + data_idx * FOURKB);
+      //increment and decrement values accordingly
+      offset_bytes++;
+      bytes_read++;
+      i++;
+      cpy_length--;
+    }
+    //move to next data block to be read and reset what byte we are at since its a new block
+    offset_blocks++;
+    offset_bytes = 0;
   }
+  return bytes_read;
 }
+
+int32_t fopen(const uint8_t* fname)
+{
+   int32_t check;
+   //copies file given by fname into global dentry
+   check = read_dentry_by_name(fname, &global_dentry);
+
+   if(check == -1)  //check if it found the file
+   {
+     return -1;
+   }
+
+   if(global_dentry.file_type != 2) //check if file type is a file
+   {
+     return -1;
+   }
+
+  return 0;
+}
+int32_t fclose(const uint8_t* fname)
+{
+  return 0;
+}
+int32_t fwrite(const uint8_t* fname)
+{
+  return -1;
+}
+// int32t fread()
+// {
+//
+// }
+int32_t dopen(const uint8_t* fname)
+{
+  int32_t check;
+  //copies file given by fname into global dentry
+  check = read_dentry_by_name(fname, &global_dentry);
+
+  if(check == -1)   //check if it found the file
+  {
+    return -1;
+  }
+
+  if(global_dentry.file_type != 1)  //check if file type is a directory
+  {
+    return -1;
+  }
+
+ return 0;
+}
+
+int32_t dclose()
+{
+  return 0;
+}
+int32_t dwrite()
+{
+  return -1;
+}
+// int32t dread()
+// {
+//
+// }
--- student-distrib/files.h
+++ student-distrib/files.h
@@ -3,6 +3,8 @@
 #ifndef _FILES_H
 #define _FILES_H
 
+#include "types.h"
+
 #define FAILURE       -1
 #define SUCCESS        0
 #define RESERVED32B    32
@@ -15,8 +17,14 @@
 
 
 
+typedef struct dentry_t { // 1 * 32 + 4 + 4 + 24 = 64 bytes in size
+  char file_name[RESERVED32B];     //Each directory entry gives a name (up to 32 characters, zero-padded)
+  uint32_t file_type;              //0 for RTC, 1 for directory, 2 for file
+  uint32_t num_inodes;             // only relevant for regular files
+  uint8_t reserved[RESERVED24B];   //24 bytes reserved
+} dentry_t;
 
-typedef struct{
+typedef struct boot_block_t {
   uint32_t num_dir_entries;     //file system statistics
   uint32_t num_inodes;
   uint32_t num_data_blocks;
@@ -24,16 +32,20 @@ typedef struct{
   dentry_t dir_entries[FILESMAX];      //both statistics and each directory occupy 64B
                                      //so the system can hold 63 files
 } boot_block_t;
-
-typedef struct{ // 1 * 32 + 4 + 4 + 24 = 64 bytes in size
-  char file_name[RESERVED32B];     //Each directory entry gives a name (up to 32 characters, zero-padded)
-  uint32_t file_type;              //0 for RTC, 1 for directory, 2 for file
-  uint32_t num_inodes;             // only relevant for regular files
-  uint8_t reserved[RESERVED24B];   //24 bytes reserved
-} dentry_t;
-
-typedef struct{
+typedef struct inode_t {
   uint32_t length_in_B;
   uint32_t data_block_idx[FILL4KB];
 } inode_t;
+
+
+int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry);
+int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry);
+int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
+extern int32_t fopen(const uint8_t* fname);
+extern int32_t fclose(const uint8_t* fname);
+extern int32_t fwrite(const uint8_t* fname);
+extern int32_t dopen(const uint8_t* fname);
+extern int32_t dclose();
+extern int32_t dwrite();
+
 #endif
